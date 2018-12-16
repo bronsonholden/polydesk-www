e543e91b956c29b2ee4f9df9ee3690f35fa00ae4
Paul Holden <paul@holdensoftware.com>
2018-12-16 15:44:43 -0800

    Fix auth dialog data

--- src/app/auth-dialog/auth-dialog.component.ts
+++ src/app/auth-dialog/auth-dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
+export interface AuthData {
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-auth-dialog',
   templateUrl: './auth-dialog.component.html',
@@ -10,7 +15,7 @@ export class AuthDialogComponent {
 
   constructor(
     public dialogRef: MatDialogRef<AuthDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+    @Inject(MAT_DIALOG_DATA) public data: AuthData) { }
 
   onClose() {
     this.dialogRef.close();
