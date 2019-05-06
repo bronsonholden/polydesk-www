428ff93bba1d25bd5133a10a90b1e68581e5b97a
Paul Holden <paul@holdensoftware.com>
2019-05-05 23:52:48 -0700

    Styling changes to uploader items

--- src/app/document-create/document-create.component.ts
+++ src/app/document-create/document-create.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
 import { AngularTokenService } from 'angular-token';
 import { ActivatedRoute } from '@angular/router';
+import { map } from  'rxjs/operators';
 
 export class FileUpload {
   data: File;
   progress: number;
+  status: string;
+};
+
+enum FileUploadStatus {
+  Pending = 1,
+  InProgress,
+  Success,
+  Failure
 };
 
 @Component({
@@ -16,7 +25,9 @@ export class FileUpload {
 export class DocumentCreateComponent implements OnInit {
 
   @ViewChild('fileUploader') fileUploader: ElementRef;
-  public selectedFiles: Array<FileUpload> = [];
+  public queuedFiles: Array<FileUpload> = [];
+  public processQueue = false;
+  private uploadingIndex = 0;
 
   constructor(private httpClient: HttpClient,
               private route: ActivatedRoute,
@@ -30,7 +41,8 @@ export class DocumentCreateComponent implements OnInit {
   }
 
   clearFiles() {
-    this.selectedFiles = [];
+    this.queuedFiles = [];
+    this.uploadingIndex = 0;
   }
 
   selectFiles(event) {
@@ -39,28 +51,72 @@ export class DocumentCreateComponent implements OnInit {
     for (let i = 0; i < event.target.files.length; ++i) {
       let file = event.target.files[i];
 
-      this.selectedFiles.push({
+      this.queuedFiles.push({
         data: file,
         progress: 0,
-        name: file.name
+        name: file.name,
+        status: FileUploadStatus.Pending
       });
     }
   }
 
-  uploadFiles() {
+  getNextFile() {
+    if (!this.processQueue || this.uploadingIndex >= this.queuedFiles.length) {
+      return null;
+    }
+
+    let file = this.queuedFiles[this.uploadingIndex];
+
+    this.uploadingIndex += 1;
+
+    return file;
+  }
+
+  startUploads() {
+    this.processQueue = true;
+    this.uploadingIndex = 0;
+
+    this.uploadNextFile();
+  }
+
+  stopUploads() {
+    this.processQueue = false;
+  }
+
+  uploadNextFile() {
+    let file = this.getNextFile();
+
+    if (file) {
+      file.status = FileUploadStatus.InProgress;
+
+      this.uploadFile(file).subscribe(event => {
+        if (event.type == HttpEventType.UploadProgress) {
+          let progress = Math.round(100 * event.loaded / event.total);
+          file.progress = progress;
+        } else if (event instanceof HttpResponse) {
+          console.log(event);
+          file.status = FileUploadStatus.Success;
+          this.uploadNextFile();
+        }
+      }, result => {
+        console.log(result);
+        file.status = FileUploadStatus.Failure;
+      });
+    }
+  }
+
+  uploadFile(file) {
     const base = this.tokenService.tokenOptions.apiBase;
     const accountIdentifier = this.route.snapshot.root.children[0].params.account;
 
-    let file = this.selectedFiles[0];
     let formData = new FormData();
 
     formData.append('content', file.data);
     formData.append('name', file.name);
 
-    this.httpClient.post(`${base}/${accountIdentifier}/documents`, formData).subscribe(result => {
-      console.log(result);
-    }, result => {
-      console.log(result);
+    return this.httpClient.post(`${base}/${accountIdentifier}/documents`, formData, {
+      reportProgress: true,
+      observe: 'events'
     });
   }
 
