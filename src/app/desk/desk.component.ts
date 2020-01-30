27477d90e25c10f79c66c51f6a7e538cdc32cc64
Paul Holden <paul@holdensoftware.com>
2020-01-29 18:31:46 -0800

    List Blueprints in account

--- src/app/desk/desk.component.ts
+++ src/app/desk/desk.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { BlueprintApiService } from '../blueprint-api.service';
 
 @Component({
   selector: 'app-desk',
@@ -7,7 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeskComponent implements OnInit {
 
-  constructor() { }
+  selection: any = [];
+  scope: any = {};
+  data: any = {
+    select: 'multiple',
+    columns: {
+      id: {
+        title: 'ID',
+        display: 'text',
+        type: 'id'
+      },
+      name: {
+        title: 'Name',
+        display: 'link',
+        type: 'concat',
+        value: {
+          separator: '/',
+          parts: [
+            {
+              type: 'literal',
+              value: 'desk'
+            },
+            {
+              type: 'attribute',
+              value: 'namespace'
+            }
+          ]
+        },
+        link: {
+          display: 'text',
+          type: 'attribute',
+          value: 'name'
+        }
+      }
+    },
+    display: [
+      {
+        name: 'id',
+        width: 60,
+        resizeable: false,
+        sortable: true
+      },
+      {
+        name: 'name',
+        width: 150,
+        resizeable: true,
+        sortable: true
+      }
+    ]
+  };
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private location: Location,
+              private blueprintApi: BlueprintApiService) { }
 
   ngOnInit() {
   }
