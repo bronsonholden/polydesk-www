53d3091b3e49a1342873e389220f6c05601f0d78
Paul Holden <paul@holdensoftware.com>
2020-02-10 21:36:45 -0800

    Fix weirdness surrounding two-way binding for prefab forms

--- src/app/prefab/prefab-form/prefab-form-container/prefab-form-container.component.ts
+++ src/app/prefab/prefab-form/prefab-form-container/prefab-form-container.component.ts
@@ -16,44 +16,14 @@ export class PrefabFormContainerComponent implements OnInit {
   form = new FormGroup({});
   fields: FormlyFieldConfig[] = [];
 
-  _model: any = {};
-  _initialModelChange = true;
-
   constructor() { }
 
   ngOnInit() {
-    Object.assign(this._model, this.model);
-
-    // Not sure how best to do this. This is done so that changes to the bound
-    // model (_model property for this component) are ignored until each
-    // Formly field has a chance to insert its initial value (undefined) at
-    // each key. Causes ExpressionChangedAfterItHasBeenCheckedError errors
-    // otherwise.
-    setTimeout(() => {
-      this._initialModelChange = false;
-    }, 100);
   }
 
   ngOnChanges(changes) {
     if (changes.layout && !changes.layout.firstChange) {
       this.fields = [Object.assign({}, this.layout)];
-    }
-
-    // In case fields is changed in the same tick, wait until next tick to
-    // update form group value so form controls will have been created.
-    if (changes.model && !changes.model.firstChange) {
-      setTimeout(() => {
-        this.form.setValue(changes.model.currentValue);
-      }, 0);
-    }
-  }
-
-  _modelChange(model) {
-    // Don't emit the first event which is emitted as soon as the model
-    // (_model property for this component) is bound to the FormlyForm
-    // component.
-    if (!this._initialModelChange) {
-      this.modelChange.emit(model);
     }
   }
 
