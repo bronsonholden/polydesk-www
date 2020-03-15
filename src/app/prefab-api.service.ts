99e57425ee6dd79d143cd4478d7798b7cccffaec
Paul Holden <paul@holdensoftware.com>
2020-03-15 15:41:39 -0700

    Create base API service
    Will expand on this to DRY up other API services.

--- src/app/prefab-api.service.ts
+++ src/app/prefab-api.service.ts
@@ -1,37 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { ResourceApi } from './resource-api';
 import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PrefabApiService {
+export class PrefabApiService extends ResourceApi{
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    super(httpClient);
+  }
+
+  resourceName() {
+    return 'prefabs';
+  }
 
   getPrefabByUid(namespace, tag) {
     return this.index(0, 1, null, { filter: { namespace, tag } });
   }
 
-  index(offset, limit, sort, query) {
-    let params = new HttpParams().set('page[offset]', offset).set('page[limit]', limit);
-
-    if (sort && sort.length > 0) {
-      params = params.set('sort', sort);
-    }
-
-    const filter = (query.filter || {});
-
-    for (let f in filter) {
-      params = params.set(`filter[${f}]`, filter[f]);
-    }
-
-    const generate = (query.generate || {});
-
-    for (let g in generate) {
-      params = params.set(`generate[${g}]`, generate[g]);
-    }
-
-    return this.httpClient.get('prefabs', { params });
-  }
 }
---
+++ src/app/resource-api.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
+export abstract class ResourceApi {
+
+  constructor(private httpClient: HttpClient) { }
+
+  abstract resourceName(): string;
+
+  index(offset, limit, sort, query) {
+    let params = new HttpParams().set('page[offset]', offset).set('page[limit]', limit);
+
+    if (sort && sort.length > 0) {
+      params = params.set('sort', sort);
+    }
+
+    const hashParams = [
+      'filter',
+      'generate'
+    ];
+
+    for (let param of hashParams) {
+      const hash = query[param] || {};
+      for (let key in hash) {
+        params = params.set(`${param}[${key}]`, hash[key]);
+      }
+    }
+
+    return this.httpClient.get(this.resourceName(), { params });
+  }
+
+}
