9df0decc66b752c35c512bc068c17fba5193728e
Paul Holden <paul@holdensoftware.com>
2020-03-29 00:25:00 -0700

    Fix Blueprint API service index method

--- src/app/blueprint-api.service.ts
+++ src/app/blueprint-api.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { ResourceApi } from './resource-api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BlueprintApiService {
+export class BlueprintApiService extends ResourceApi {
 
   constructor(private httpClient: HttpClient) { }
 
-  index(offset, limit, sort, filter) {
-    let params = new HttpParams().set('page[offset]', offset).set('page[limit]', limit);
-
-    if (sort && sort.length > 0) {
-      params = params.set('sort', sort);
-    }
-
-    for (let f in filter) {
-      params = params.set(`filter[${f}]`, filter[f]);
-    }
-
-    return this.httpClient.get('blueprints', { params });
+  resourceName(): string {
+    return 'blueprints';
   }
 
   getBlueprint(id) {
